cf7f9c8f1f1ce00c8a600628d266eccd4011528c
pilipuk-aliaksandr <shurapilipuk@gmail.com>
2025-03-03 23:15:02 +0300

    commit

--- src/main/java/by/pilipuk/AppMain.java
+++ src/main/java/by/pilipuk/AppMain.java
@@ -2,6 +2,6 @@ package by.pilipuk;
 
 public class AppMain {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello biches");
     }
 }
