6725a0d53d44a2580100484fe503e55e45f79df7
pilipuk-aliaksandr <shurapilipuk@gmail.com>
2025-07-03 14:13:11 +0300

    converted from spring framework to spring boot

---
+++ src/main/java/jdbc/crudApp.java
@@ -0,0 +1,11 @@
+package jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class crudApp {
+    public static void main(String[] args) {
+        SpringApplication.run(crudApp.class, args);
+    }
+}
