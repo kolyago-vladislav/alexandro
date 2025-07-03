77dc6d5528402d8cf0a3aad275dbdf4d0a8270ec
pilipuk-aliaksandr <shurapilipuk@gmail.com>
2025-07-03 14:18:36 +0300

    removed unnecessary imports and dependencies

--- src/main/java/jdbc/repository/UserRepositoryImplWithHib.java
+++ src/main/java/jdbc/repository/UserRepositoryImplWithHib.java
@@ -4,7 +4,6 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jdbc.entity.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
--- src/main/java/jdbc/service/UserServiceImpl.java
+++ src/main/java/jdbc/service/UserServiceImpl.java
@@ -2,7 +2,6 @@ package jdbc.service;
 
 import jdbc.entity.User;
 import jdbc.repository.UserRepositoryImplWithHib;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
